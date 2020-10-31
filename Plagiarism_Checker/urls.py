63ae3c199e0d16efb0c21ee100481e5e669201b7
Jinal <adhyarujinal@gmail.com>
2020-10-31 16:23:07 +0530

    created app

--- Plagiarism_Checker/urls.py
+++ Plagiarism_Checker/urls.py
@@ -1,4 +1,4 @@
-"""Plagiarism_Ckecker URL Configuration
+"""Plagiarism_Checker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
