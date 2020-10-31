3e211656f39f7fe075aed6dcd8a0c5007d086ebe
Jinal <adhyarujinal@gmail.com>
2020-10-31 16:12:32 +0530

    created app

--- Plagiarism_Ckecker/settings.py
+++ Plagiarism_Ckecker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'plagiarismchecker.apps.PlagiarismcheckerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
