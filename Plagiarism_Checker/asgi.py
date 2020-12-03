708c41f27f157abc02f127c8d95f1571a884a8e6
Jinal <adhyarujinal@gmail.com>
2020-12-03 11:49:24 +0530

    changed some part of front end

--- Plagiarism_Checker/asgi.py
+++ Plagiarism_Checker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Plagiarism_Ckecker project.
+ASGI config for Plagiarism_Checker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Plagiarism_Ckecker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Plagiarism_Checker.settings')
 
 application = get_asgi_application()
