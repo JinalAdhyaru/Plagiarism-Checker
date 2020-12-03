becba4d50c4d987f41a0553f48930bd01f7f3ff6
unknown <naik.khushi5423@gmail.com>
2020-12-03 18:13:37 +0530

    frntend update

--- plagiarismchecker/views.py
+++ plagiarismchecker/views.py
@@ -20,7 +20,7 @@ def test(request):
     if request.POST['q'] != '' : 
         print("hurray its q")
         percent,link = main.findSimilarity(request.POST['q'])
-    
+        percent = round(percent,2)
     print("Output>>>>>>>>>>>>>>>>>>>>!!!!!!!!",percent,link)
     return render(request, 'pc/index.html',{'link': link, 'percent': percent})
 
