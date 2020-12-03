532609c34496ba3cc4daa55e1a9f6df1decfcb02
Jinal <adhyarujinal@gmail.com>
2020-12-03 12:25:47 +0530

    removed unused files

--- plagiarismchecker/views.py
+++ plagiarismchecker/views.py
@@ -11,17 +11,12 @@ def test(request):
     print("request is welcome test")
     print(request.POST['q'])
     
-    '''value = ''
-    #if !request.POST['q'] :
-    if "txt" in str(request.FILES['docfile']):
-        value = str(request.FILES['docfile'].read())
-    '''
-    '''if request.POST['q'] != '' : 
+    if request.POST['q'] != '' : 
         print("hurray its q")
         result = main.findSimilarity(request.POST['q'])
-    '''
-    #print("Output>>>>>>>>>>>>>>>>>>>>!!!!!!!!", result)
-    #return render(request, 'pc/index.html',{'result': result})
+   
+    print("Output>>>>>>>>>>>>>>>>>>>>!!!!!!!!", result)
+    return render(request, 'pc/index.html',{'result': result })
 
 def filetest(request):
     value = ''    
@@ -30,7 +25,8 @@ def filetest(request):
         value = str(request.FILES['docfile'].read())
     
     print(value)
-    return render(request, 'pc/index.html')
+    result = main.findSimilarity(value)
+    return render(request, 'pc/index.html',{'result' : result})
   
 
 def fileCompare(request):
