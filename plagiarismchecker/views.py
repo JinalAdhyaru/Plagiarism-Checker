9e3d969e20f6c2f71149944f2abbcf92c2f70d25
jahnavi0801 <jahnavi0801@gmail.com>
2020-12-03 18:21:25 +0530

    Docx added

--- plagiarismchecker/views.py
+++ plagiarismchecker/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from plagiarismchecker.algorithm import main
+from docx import *
+from PyPDF2 import PdfFileReader
+
+
 #import matplotlib.pyplot as plt
 #from plagiarismchecker.algorithm import main
 
@@ -12,13 +16,7 @@ def test(request):
     print("request is welcome test")
     print(request.POST['q'])  
     
-    '''value = ''
-    #if !request.POST['q'] :
-    if "txt" in str(request.FILES['docfile']):
-        value = str(request.FILES['docfile'].read())
-    '''
-    if request.POST['q'] != '' : 
-        print("hurray its q")
+    if request.POST['q']: 
         percent,link = main.findSimilarity(request.POST['q'])
     
     print("Output>>>>>>>>>>>>>>>>>>>>!!!!!!!!",percent,link)
@@ -27,10 +25,21 @@ def test(request):
 def filetest(request):
     value = ''    
     print(request.FILES['docfile'])
-    if "txt" in str(request.FILES['docfile']):
+    if str(request.FILES['docfile']).endswith(".txt"):
         value = str(request.FILES['docfile'].read())
+
+    elif str(request.FILES['docfile']).endswith(".docx"):
+        document = Document(request.FILES['docfile'])
+        for para in document.paragraphs:
+            value += para.text
     
-    print(value)
+    elif str(request.FILES['docfile']).endswith(".pdf"):
+        pdfFileObj = open(str(request.FILES['docfile']), 'r') 
+        print(pdfFileObj)
+
+        
+    percent,link = main.findSimilarity(value)
+    print("Output>>>>>>>>>>>>>>>>>>>>!!!!!!!!",percent,link)
     return render(request, 'pc/index.html')
   
 
