b8665620477c60ef6340da8d4480c2275235fbca
Prateek9144 <prateekchouhan00@gmail.com>
2020-12-03 14:29:15 +0530

    nltk package add

--- plagiarismchecker/views.py
+++ plagiarismchecker/views.py
@@ -11,12 +11,17 @@ def test(request):
     print("request is welcome test")
     print(request.POST['q'])
     
-    if request.POST['q'] != '' : 
+    '''value = ''
+    #if !request.POST['q'] :
+    if "txt" in str(request.FILES['docfile']):
+        value = str(request.FILES['docfile'].read())
+    '''
+    '''if request.POST['q'] != '' : 
         print("hurray its q")
         result = main.findSimilarity(request.POST['q'])
-   
-    print("Output>>>>>>>>>>>>>>>>>>>>!!!!!!!!", result)
-    return render(request, 'pc/index.html',{'result': result })
+    '''
+    #print("Output>>>>>>>>>>>>>>>>>>>>!!!!!!!!", result)
+    #return render(request, 'pc/index.html',{'result': result})
 
 def filetest(request):
     value = ''    
@@ -25,8 +30,7 @@ def filetest(request):
         value = str(request.FILES['docfile'].read())
     
     print(value)
-    result = main.findSimilarity(value)
-    return render(request, 'pc/index.html',{'result' : result})
+    return render(request, 'pc/index.html')
   
 
 def fileCompare(request):
