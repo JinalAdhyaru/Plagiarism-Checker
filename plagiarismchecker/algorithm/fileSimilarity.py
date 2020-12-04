19275505e0825e6507c1d9c10a955ddc96b93aec
Prateek9144 <prateekchouhan00@gmail.com>
2020-12-04 11:05:50 +0530

    nltk package added in fileSimilarirty

--- plagiarismchecker/algorithm/fileSimilarity.py
+++ plagiarismchecker/algorithm/fileSimilarity.py
@@ -1,75 +1,78 @@
 import re
 import math
+from nltk.corpus import stopwords
 
 
-def findFileSimilarity(database, inputQuery):
+def findFileSimilarity(inputQuery, database):
 
-    universalSetOfUniqueWords = []
-    matchPercentage = 0
+	universalSetOfUniqueWords = []
+	matchPercentage = 0
 
-    lowercaseQuery = inputQuery.lower()
+	lowercaseQuery = inputQuery.lower()
+	en_stops = set(stopwords.words('english'))
 
-    # Replace punctuation by space and split
-    queryWordList = re.sub("[^\w]", " ", lowercaseQuery).split()
-    
-    for word in queryWordList:
-        if word not in universalSetOfUniqueWords:
-            universalSetOfUniqueWords.append(word)
+	# Replace punctuation by space and split
+	queryWordList = re.sub("[^\w]", " ", lowercaseQuery).split()
+	# queryWordList = map(str, queryWordList)					#This was causing divide by zero error
 
-    database1 = database.lower()
+	for word in queryWordList:
+		if word not in universalSetOfUniqueWords:
+			universalSetOfUniqueWords.append(word)
 
-    # Replace punctuation by space and split
-    databaseWordList = re.sub("[^\w]", " ", database1).split()
-    # databaseWordList = map(str, databaseWordList)			#And this also leads to divide by zero error
+	database1 = database.lower()
 
-    for word in databaseWordList:
-        if word not in universalSetOfUniqueWords:
-            universalSetOfUniqueWords.append(word)
+	# Replace punctuation by space and split
+	databaseWordList = re.sub("[^\w]", " ", database1).split()
+	# databaseWordList = map(str, databaseWordList)			#And this also leads to divide by zero error
 
-    queryTF = []
-    databaseTF = []
+	for word in databaseWordList:
+		if word not in universalSetOfUniqueWords:
+			universalSetOfUniqueWords.append(word)
 
-    for word in universalSetOfUniqueWords:
-        queryTfCounter = 0
-        databaseTfCounter = 0
+	for word in universalSetOfUniqueWords:
+		if word in en_stops:
+			universalSetOfUniqueWords.remove(word)
 
-        for word2 in queryWordList:
-            if word == word2:
-                queryTfCounter += 1
-        queryTF.append(queryTfCounter)
+	queryTF = []
+	databaseTF = []
 
-        for word2 in databaseWordList:
-            if word == word2:
-                databaseTfCounter += 1
-        databaseTF.append(databaseTfCounter)
+	for word in universalSetOfUniqueWords:
+		queryTfCounter = 0
+		databaseTfCounter = 0
 
-    dotProduct = 0
-    for i in range(len(queryTF)):
-        dotProduct += queryTF[i]*databaseTF[i]
+		for word2 in queryWordList:
+			if word == word2:
+				queryTfCounter += 1
+		queryTF.append(queryTfCounter)
 
-    queryVectorMagnitude = 0
-    for i in range(len(queryTF)):
-        queryVectorMagnitude += queryTF[i]**2
-    queryVectorMagnitude = math.sqrt(queryVectorMagnitude)
+		for word2 in databaseWordList:
+			if word == word2:
+				databaseTfCounter += 1
+		databaseTF.append(databaseTfCounter)
 
-    databaseVectorMagnitude = 0
-    for i in range(len(databaseTF)):
-        databaseVectorMagnitude += databaseTF[i]**2
-    databaseVectorMagnitude = math.sqrt(databaseVectorMagnitude)
+	dotProduct = 0
+	for i in range(len(queryTF)):
+		dotProduct += queryTF[i]*databaseTF[i]
 
-    matchPercentage = (float)(
-        dotProduct / (queryVectorMagnitude * databaseVectorMagnitude))*100
+	queryVectorMagnitude = 0
+	for i in range(len(queryTF)):
+		queryVectorMagnitude += queryTF[i]**2
+	queryVectorMagnitude = math.sqrt(queryVectorMagnitude)
 
-    print(matchPercentage)
-    return matchPercentage
+	databaseVectorMagnitude = 0
+	for i in range(len(databaseTF)):
+		databaseVectorMagnitude += databaseTF[i]**2
+	databaseVectorMagnitude = math.sqrt(databaseVectorMagnitude)
+
+	matchPercentage = (float)(
+		dotProduct / (queryVectorMagnitude * databaseVectorMagnitude))*100
+
+# 	print (universalSetOfUniqueWords)
+# 	print()
+# 	print (databaseWordList)
 
 
-def multipleFile(text, database):
-    max = 0
-    maxSimFile = ''
-    for file in database:
-        sim = findFileSimilarity(text, database[file])
-        if(sim > max):
-            max = sim
-            maxSimFile = file
-    return maxSimFile, max
+# 	print (queryTF)
+# 	print (databaseTF)
+
+	return matchPercentage
